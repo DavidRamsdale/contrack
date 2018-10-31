6d9bbd150f4502c493557f877385741c720c7bf0
David Ramsdale <davidramsdale1991@gmail.com>
2018-10-31 14:42:05 +1100

    Fixed enum bug and added the authorisation functionality such that users can't update other users
    Co-authored-by: Scott O'Dea <odeascott@gmail.com>

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -1,8 +1,8 @@
 class Contractor < ApplicationRecord
   belongs_to :user
 
-  enum lvl_of_exp: {"junior" => 0, "mid" => 1, "senior" => 2}
-  enum engineer_type: {"front end" => 0, "back end" => 1, "full stack" => 2}
+  enum lvl_of_exp: {"Junior" => 0, "Mid" => 1, "Senior" => 2}
+  enum engineer_type: {"Front end" => 0, "Back end" => 1, "Full stack" => 2}
   enum pay_range: {"0-200" => 0, "200-400" => 1, "400-600" => 2, "600-800" => 3, "800-1000" => 4, "1000+" => 5 }
   enum pref_contract: {"3 months" => 0 , "6 months" => 1, "9 months" => 2, "12 months" => 3}
 end
