5abe199e010cdf9148d8395485f8ba643e390fa4
Carl Gray <carl@firstfnf.com>
2018-11-02 17:49:06 +1100

    added payment stripe

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+
+  
   def index 
     if user_signed_in?
       if current_user.is_contractor == false and current_user.recruiter == nil
@@ -12,4 +14,31 @@ class HomeController < ApplicationController
       end
     end
   end
+
+
+  def process_payment
+  # Amount in cents
+  @amount = 500
+
+  customer = Stripe::Customer.create(
+    :email => params[:stripeEmail],
+    :source  => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :customer    => customer.id,
+    :amount      => @amount,
+    :description => 'Rails Stripe customer',
+    :currency    => 'aud'
+  )
+
+  current_user.recruiter.paid = true
+  current_user.recruiter.save  
+  redirect_to '/'
+
+rescue Stripe::CardError => e
+  flash[:error] = e.message
+  redirect_to new_charge_path
 end
+  end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :recruiters
   devise_for :users
   root to: "home#index"
+  post '/process_payment', to: 'home#process_payment', as: 'payment'
+  get '/recruiter/payment', to: 'recruiters#payment', as: 'recruiterpayment'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
