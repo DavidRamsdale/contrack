314654024e3d8ba66f0c68448e8691e8b2d1dc44
David Ramsdale <davidramsdale1991@gmail.com>
2018-11-05 17:20:33 +1100

    Added in the search functions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,20 @@ class SearchController < ApplicationController
   def results
     
     # @basic_search = params[:lvl_of_exp]
-    @basic_search = Contractor.basic_search(lvl_of_exp: "#{params[:lvl_of_exp]}")
+    
+    @basic_search = Contractor.basic_search(lvl_of_exp: "#{params[:lvl_of_exp]}", engineer_type: "#{params[:engineer_type]}", city: "#{params[:city]}", pay_range: "#{params[:pay_range]}", pref_contract: "#{params[:pref_contract]}").collect(&:user_id)
+    
+    # params[:language_id]
+    @array =[]
+    @language_search = params[:language_id]
+    @language_search.each do |x|
+      @array << Language.find(x).users.pluck(:user_id)
+    end
   end
+
+
+  # def language_user(x)
+  #   Language.find(x).users.pluck(:user_id)
+  # end
 end
+
