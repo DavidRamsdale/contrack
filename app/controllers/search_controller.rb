3fd59e98aa2e954260598ed4af5154c4f4c9bdc9
David Ramsdale <davidramsdale1991@gmail.com>
2018-11-06 10:40:43 +1100

    Completed the search functions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,25 +2,18 @@ class SearchController < ApplicationController
   def results
     
     # @basic_search = params[:lvl_of_exp]
-    
+    byebug
     @basic_search = Contractor.basic_search(lvl_of_exp: "#{params[:lvl_of_exp]}", engineer_type: "#{params[:engineer_type]}", city: "#{params[:city]}", pay_range: "#{params[:pay_range]}", pref_contract: "#{params[:pref_contract]}").collect(&:user_id)
-    
-    # params[:language_id]
+    @date = Contractor.where("end_date <= ?", "#{params[:end_date]}").collect(&:user_id)
+    @search_with_date =  @basic_search & @date 
     @array = []
     @language_search = params[:language_id]
     @language_search.each do |x|
       @array << Language.find(x).users.pluck(:user_id)
     end
     @result = @array.inject(:&)
-    @end = @basic_search & @result
+    @end = @search_with_date & @result
     
-    @end.each do |z|
-      @user = User.find(z) 
-    end
-
-    
-
-
   end
 
 
