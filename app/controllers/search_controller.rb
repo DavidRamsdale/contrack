46d3db56cb5e41eb3b3582bce088cf955c78f2a8
Scott O'Dea <odeascott@gmail.com>
2018-11-06 10:45:18 +1100

    basic styling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def results
     byebug
-    @basic_search = Contractor.basic_search(lvl_of_exp: "1")
+    @basic_search = Contractor.basic_search(city: "#{params[:city]}")
   end
 end
